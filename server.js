f30e9b382aeadfc22262767664b9ec7c5d227e60
kishlay123 <kishlaykashish99@gmail.com>
2021-12-12 10:57:57 +0530

    initialised commit

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 // let's start!!!! yoo
 const express= require('express');
 const exphbs= require('express-handlebars');
+const bodyParser= require('body-parser')
+const mongoose= require('mongoose');
 const app = express();
+// using body parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 // setup view engine
 app.engine('handlebars',exphbs({defaultLayout:'main'}));
 app.set('view engine','handlebars');
+// env variable
 const PORT= process.env.PORT||8000;
 // app.use('json');
 app.get('/',(req,res)=>{
@@ -21,6 +27,9 @@ app.get('/contact',(req, res)=>{
     res.render('contact',{
         title: 'Contact'
     });
+})
+app.post('/contactUs',(req,res)=>{
+    console.log(req.body);
 })
 app.listen(PORT, ()=>{
     console.log(`server running on ${PORT}`);
